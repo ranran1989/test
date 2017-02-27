e5220e318707e638be76d5b75170a3b35f2fe297
zhaoranran <zhaoranran@weiresearch.com>
2017-02-28 00:30:12 +0800

    first test

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8080;
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(port);
